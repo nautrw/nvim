db04ce09344abdca8c513a2256618b5e03ced29b
nautrw <nautrw@proton.me>
2023-12-26 17:40:07 -0500

    feat: add chadrc.lua and init.lua
    Essentially, the base of the config was added. This will likely be extended in the future.

---
+++ custom/chadrc.lua
@@ -0,0 +1,31 @@
+---@type ChadrcConfig
+
+local M = {}
+
+M.ui = {
+    theme = 'rosepine',
+    tabufline = {
+        enabled = false,
+        lazyload = false,
+    },
+    statusline = {
+        theme = 'vscode_colored'
+    },
+    nvdash = {
+        load_on_startup = true,
+        header = {
+            [[█▀█ █   █ █ █▀   █ █ █   ▀█▀ █▀█ ▄▀█]],
+            [[█▀▀ █▄▄ █▄█ ▄█   █▄█ █▄▄  █  █▀▄ █▀█]]
+        },
+        buttons = {
+            { "  Find File", "Spc f f", "Telescope find_files" },
+            { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+            { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+            { "  Bookmarks", "Spc m a", "Telescope marks" },
+            { "  Themes", "Spc t h", "Telescope themes" },
+            { "  Mappings", "Spc c h", "NvCheatsheet" },
+        },
+    },
+}
+
+return M
