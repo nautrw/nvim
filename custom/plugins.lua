4df83454e048043faf9a4aa90094ec74dfa709f9
nautrw <nautrw@proton.me>
2023-12-26 19:54:03 -0500

    feat: add python linters with none-ls
    Due to the receDue to the recent archiving of null-ls, a new community-run version was created. This version is the same as null-ls, but it just goes by the name "none-ls."

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -3,7 +3,9 @@ local plugins = {
         'williamboman/mason.nvim',
         opts = {
             ensure_installed = {
-                'pyright'
+                'pyright',
+                'mypy',
+                'ruff',
             }
         }
     },
@@ -12,6 +14,13 @@ local plugins = {
         config = function()
             require('plugins.configs.lspconfig')
             require('custom.configs.lspconfig')
+        end,
+    },
+    {
+        'nvimtools/none-ls.nvim', -- Replacement for null-ls
+        ft = { 'python' },
+        config = function()
+            return require('custom.configs.null-ls')
         end,
     }
 }
