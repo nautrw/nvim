69710bd25b53d6e6e9749d94d65400e7c876f28a
nautrw <nautrw@proton.me>
2023-12-26 19:56:35 -0500

    feat: add treesitter highlighters

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -22,6 +22,28 @@ local plugins = {
         config = function()
             return require('custom.configs.null-ls')
         end,
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        opts = {
+            ensure_installed = {
+                "json",
+                "javascript",
+                "typescript",
+                "tsx",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "dockerfile",
+                "gitignore",
+                'python'
+            },
+        }
     }
 }
 
