25182e816e12c8aed3147a0e1e85a33e4fe1f9db
nautrw <nautrw@proton.me>
2023-12-28 13:32:21 -0500

    feat: use 4 space indentation instead of 2-space

--- custom/init.lua
+++ custom/init.lua
@@ -1,6 +1,6 @@
 -- Tabs
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
