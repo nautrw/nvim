1e144602a2b461f1376b8c31c0997503b73ff412
nautrw <nautrw@proton.me>
2024-07-08 02:05:22 -0400

    change statusline theme and disable tabufline
    Tabufline gives me anxiety

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,18 @@
 local M = {}
 
 M.ui = {
-    theme = "gruvbox",
+    theme = "flexoki",
     nvdash = {
         load_on_startup = true,
         header = {
             [[ El corazón tranquilo da vida al cuerpo. [✝] Proverbios 14:30 ]]
         },
+    },
+    statusline = {
+        theme = "vscode_colored"
+    },
+    tabufline = {
+        enabled = false,
     }
 }
 
