e83ac35affd2a0ae70a9f29b42953a7fef029cee
nautrw <nautrw@proton.me>
2024-07-08 16:10:40 -0400

    feat: add tresitteer and mason plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,32 +6,24 @@ return {
             require "configs.conform"
         end,
     },
-    -- These are some examples, uncomment them if you want to see them work!
-    -- {
-    --   "neovim/nvim-lspconfig",
-    --   config = function()
-    --     require("nvchad.configs.lspconfig").defaults()
-    --     require "configs.lspconfig"
-    --   end,
-    -- },
-    --
-    -- {
-    -- 	"williamboman/mason.nvim",
-    -- 	opts = {
-    -- 		ensure_installed = {
-    -- 			"lua-language-server", "stylua",
-    -- 			"html-lsp", "css-lsp" , "prettier"
-    -- 		},
-    -- 	},
-    -- },
-    --
-    -- {
-    -- 	"nvim-treesitter/nvim-treesitter",
-    -- 	opts = {
-    -- 		ensure_installed = {
-    -- 			"vim", "lua", "vimdoc",
-    --      "html", "css"
-    -- 		},
-    -- 	},
-    -- },
+    {
+        'williamboman/mason.nvim',
+        opts = {
+            ensure_installed = {
+                -- Lua
+                "lua-language-server",
+                'stylua'
+            }
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                'lua',
+                'vimdoc',
+                'vim'
+            }
+        }
+    }
 }
