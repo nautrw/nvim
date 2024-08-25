b521f3d472b15ac8c4a0926b2c145a3fa242db8c
nautrw <nautrw@proton.me>
2024-08-25 18:20:50 -0400

    add lazygit

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,11 +9,13 @@ vim.keymap.set("i", "<A-l>", "<Right>", { desc = "move right" })
 vim.keymap.set("i", "<A-j>", "<Down>", { desc = "move down" })
 vim.keymap.set("i", "<A-k>", "<Up>", { desc = "move up" })
 
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
 vim.keymap.set("n", "<leader>nk", ":nohl<CR>", { desc = "Clear search highlights" })
+
+vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "open lazygit" })
 
 vim.keymap.del("n", "<leader>n")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,4 +27,8 @@ return {
             require("colorizer").setup()
         end,
     },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = false,
+    },
 }
