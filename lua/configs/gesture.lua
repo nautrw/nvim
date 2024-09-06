66af97c2a567f7376667e0dbfa9c32f65b936e3d
nautrw <nautrw@proton.me>
2024-09-05 21:03:16 -0400

    make scrolling gestures more intuitive

--- lua/configs/gesture.lua
+++ lua/configs/gesture.lua
@@ -2,13 +2,13 @@ local gesture = require "gesture"
 
 gesture.register {
     name = "scroll to bottom",
-    inputs = { gesture.up(), gesture.down() },
+    inputs = { gesture.down(), gesture.up() },
     action = "normal! G",
 }
 
 gesture.register {
     name = "scroll to top",
-    inputs = { gesture.down(), gesture.up() },
+    inputs = { gesture.up(), gesture.down() },
     action = "normal! gg",
 }
 
