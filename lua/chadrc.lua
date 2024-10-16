0ee1d7df50ad6066d917510579267978bf461a57
nautrw <nautrw@proton.me>
2024-10-15 20:17:12 -0400

    fix nvdash

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -15,24 +15,25 @@ M.ui = {
     tabufline = {
         enabled = false, -- bufferlines give me anxiety
     },
-    nvdash = {
-        load_on_startup = true,
-        header = {
-            "Gloria al Padre, y al Hijo, y al Espíritu Santo;",
-            "Como era en el principio, ahora, y siempre, por ",
-            "los siglos de los siglos.",
-            "",
-            "────── ✝ ──────",
-            "",
-            "Glory be to the Father, and to the Son, and to the",
-            "Holy Spirit. As it was in the beginning, is now,",
-            "and shall ever be, world without end.",
-            "",
-            "Amen.",
-        },
-    },
     statusline = {
         theme = "vscode_colored",
+    },
+}
+
+M.nvdash = {
+    load_on_startup = true,
+    header = {
+        "Gloria al Padre, y al Hijo, y al Espíritu Santo;",
+        "Como era en el principio, ahora, y siempre, por ",
+        "los siglos de los siglos.",
+        "",
+        "────── ✝ ──────",
+        "",
+        "Glory be to the Father, and to the Son, and to the",
+        "Holy Spirit. As it was in the beginning, is now,",
+        "and shall ever be, world without end.",
+        "",
+        "Amen.",
     },
 }
 
