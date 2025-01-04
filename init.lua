99a7aec0fe8a9c7d4d561c4af264c27852ed5968
nautrw <nautrw@proton.me>
2025-01-04 01:45:34 -0500

    fix colorscheme background thingy

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 vim.loader.enable()
 require "config.options"
 require "config.lazy"
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        local float_border = vim.api.nvim_get_hl(0, { name = "FloatBorder" })
+        float_border.bg = "none"
+        vim.api.nvim_set_hl(0, "FloatBorder", float_border)
+    end,
+})
 require "config.persistent_colorscheme"
 require "config.mappings"
