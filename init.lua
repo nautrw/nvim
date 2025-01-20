1287461157dd2ea40923c2c7422c0934f9d899dd
nautrw <nautrw@proton.me>
2025-01-20 16:49:08 -0500

    format init.luua

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 vim.loader.enable()
 require "config.options"
 require "config.lazy"
+
 vim.api.nvim_create_autocmd("ColorScheme", {
     callback = function()
         local float_border = vim.api.nvim_get_hl(0, { name = "FloatBorder" })
@@ -8,5 +9,6 @@ vim.api.nvim_create_autocmd("ColorScheme", {
         vim.api.nvim_set_hl(0, "FloatBorder", float_border)
     end,
 })
+
 require "config.persistent_colorscheme"
 require "config.mappings"
