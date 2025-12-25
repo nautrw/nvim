e4448185cbf567d65a97bd2d7370b7dd5de00bc0
nautrw <nautrw@proton.me>
2025-12-25 10:30:19 -0500

    fix stuff

--- lua/config/plugins/fff.lua
+++ lua/config/plugins/fff.lua
@@ -1,6 +1,8 @@
 return {
     "dmtrKovalenko/fff.nvim",
-    build = "cargo build --release",
+    build = function()
+        require("fff.download").download_or_build_binary()
+    end,
     opts = {
         prompt = "󰘧 ",
         title = "Find Files",
