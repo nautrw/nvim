552b739755ae7b8296947262267a478d02643312
nautrw <nautrw@proton.me>
2026-02-25 21:02:25 -0500

    pls work

--- init.lua
+++ init.lua
@@ -4,3 +4,18 @@ require("config.options")
 require("config.keymap")
 require("config.lazy")
 require("config.persistent_colorscheme")
+
+vim.diagnostic.config {
+    virtual_text = false,
+    underline = true,
+    update_in_insert = true,
+    signs = {
+        active = true,
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "󰟃",
+            [vim.diagnostic.severity.INFO] = "",
+        },
+    },
+}
--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -28,51 +28,5 @@ return {
                 "prettier",
             },
         }
-
-        -- require("mason-nvim-dap").setup {
-        --     ensure_installed = { "python" },
-        --     automatic_installation = true,
-        --     handlers = {
-        --         function(config)
-        --             require("mason-nvim-dap").default_setup(config)
-        --         end,
-        --     },
-        -- }
-
-        vim.lsp.config("basedpyright", {
-            settings = {
-                basedpyright = {
-                    analysis = {
-                        autoSearchPaths = true,
-                        useLibraryCodeForTypes = true,
-                        typeCheckingMode = "standard",
-                        inlayHints = {
-                            callArgumentNames = true,
-                            genericTypes = true,
-                            variableTypes = true,
-                            functionReturnTypes = true,
-                        },
-                    },
-                    python = {
-                        venvPath = "./.venv",
-                    },
-                },
-            },
-        })
-
-        vim.diagnostic.config {
-            virtual_text = false,
-            underline = true,
-            update_in_insert = true,
-            signs = {
-                active = true,
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "",
-                    [vim.diagnostic.severity.WARN] = "",
-                    [vim.diagnostic.severity.HINT] = "󰟃",
-                    [vim.diagnostic.severity.INFO] = "",
-                },
-            },
-        }
-    end,
+            end,
 }
